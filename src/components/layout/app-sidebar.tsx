1248c2e05a1a4808e44373d53e32b3c7ea4aef41
Cody Winesett <cody@lilagents.com>
2026-06-25 15:04:49 -0400

    Use sidebar menu styling for search

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -1,12 +1,16 @@
+import { SearchIcon } from 'lucide-react'
 import { useLayout } from '@/context/layout-provider'
+import { useSearch } from '@/context/search-provider'
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
   SidebarRail,
 } from '@/components/ui/sidebar'
-import { Search } from '@/components/search'
 import { ThemeSwitch } from '@/components/theme-switch'
 import { AppTitle } from './app-title'
 import { sidebarData } from './data/sidebar-data'
@@ -14,11 +18,24 @@ import { NavGroup } from './nav-group'
 
 export function AppSidebar() {
   const { collapsible, variant } = useLayout()
+  const { setOpen } = useSearch()
+
   return (
     <Sidebar collapsible={collapsible} variant={variant}>
       <SidebarHeader>
         <AppTitle />
-        <Search className='mx-2 max-w-[calc(100%-1rem)] flex-none sm:w-auto lg:w-auto xl:w-auto group-data-[collapsible=icon]:mx-auto group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:px-0' />
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              tooltip='Search'
+              aria-keyshortcuts='Meta+K Control+K'
+              onClick={() => setOpen(true)}
+            >
+              <SearchIcon />
+              <span>Search</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
         {sidebarData.navGroups.map((props) => (
